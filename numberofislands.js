091b5d0c9394001e750ad5ef0c5ed101866063e8
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-22 09:13:47 -0400

    edits to solution

--- numberofislands.js
+++ numberofislands.js
@@ -5,12 +5,10 @@
 //000
 
 let array = 
-[
-[11110],
+[[11110],
 [11010],
 [11000],
-[00000]
-]
+[00000]]
 
 
 //O(n^2) solution since we need to iterate over a 2-D array
@@ -24,22 +22,23 @@ for (let i = 0; i < array.length; i++) {
     }
 }
 
-//To check if it's an island, check if it's in bounds first
-//Then, mark it as 'checked' by making it a 0
-//Finally, check whether it's neighbors are 1's as well, making them as 0's if so
-//If everything ends up as true, then it is an island
 function isIsland(grid, i, j) {
+    //To check if it's an island, check if it's in bounds first
     if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === 0) {
         return false 
     }
 
+    //Then, mark it as 'checked' by making the tile a 0
     grid[i][j] = 0
 
+    //Finally, check whether it's neighbors are 1's as well, making them as 0's
+    //in the process.
     isIsland(grid, i + 1, j)
     isIsland(grid, i - 1, j)
     isIsland(grid, i, j + 1)
     isIsland(grid, i, j - 1)
 
+    //Once the island is completed expanding in all directions, return true
     return true
 }
 
