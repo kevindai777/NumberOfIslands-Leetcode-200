f6c6c1a24cdd787e44941b67698db7754df75a6e
Kevin Dai <adamgordon@KevinDai.home>
2020-09-04 03:47:15 -0400

    solutions

---
+++ numberofislands.java
@@ -0,0 +1,32 @@
+//Java Solution
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (dfs(grid, i, j)) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private boolean dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+            return false;
+        }
+        
+        grid[i][j] = '0';
+        
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+        
+        return true;
+    }
+}
